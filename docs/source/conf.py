6c13548e225821e1be6fca549b7f3d80669817fa
Jean Carlo Hernández Arguedas <jeanca@MacBook-Pro-de-Jean.local>
2023-10-13 09:01:22 -0600

    update version test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("../../src/"))
 project = 'PyLibImgMail'
 copyright = '2023, Jean Carlo Hernández Arguedas'
 author = 'Jean Carlo Hernández Arguedas'
-release = '1.0.0'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
