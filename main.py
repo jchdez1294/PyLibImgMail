e1664a182ff268a6e2ee5bb60624fb83d3d028f6
Jean Carlo Hernández Arguedas <jeanca@MacBook-Pro-de-Jean.local>
2023-10-13 08:52:19 -0600

    update main.py

--- main.py
+++ main.py
@@ -8,13 +8,26 @@ from src.mymail import sendAttachEmail
 def main():
     """
     Función principal, prueba de ejecución de módulo.
+    Ejemplos de uso:
     """
 
-    showImageFromURL("https://logowik.com/content/uploads/images/python.jpg")
-    downloadImageFromUrl("https://logowik.com/content/uploads/images/python.jpg", "/Users/jeanca/Desktop/test")
-    grayScaleImage("/Users/jeanca/Desktop/test/image-0bf45ca6-43cb-4e4b-8a03-523d6172d4fe.png")
+    img_status = showImageFromURL("https://logowik.com/content/uploads/images/python.jpg")
+    if not img_status:
+        print("No se pudo descargar la imagen.")
 
-    sendQuickMail("Test1", "hola mundo", "jc.hdez.1294@gmail.com")
-    sendAttachEmail("Test2", "hola mundo", "jc.hdez.1294@gmail.com", "/Users/jeanca/Desktop/test/image-0bf45ca6-43cb-4e4b-8a03-523d6172d4fe.png")
+    path = downloadImageFromUrl("https://logowik.com/content/uploads/images/python.jpg", "/Users/jeanca/Desktop/test")
+    print(f"Imagen guardada en la ruta: {path}")
+    
+    img_gray_status = grayScaleImage("/Users/jeanca/Desktop/test/image-0bf45ca6-43cb-4e4b-8a03-523d6172d4fe.png")
+    if not img_gray_status:
+        print("No se pudo descargar la imagen.")
 
+
+    quick_mail_status = sendQuickMail("Test1", "hola mundo", "jc.hdez.1294@gmail.com")
+    if quick_mail_status:
+        print("Correo enviado exitosamente.")
+
+    attach_mail_status = sendAttachEmail("Test2", "hola mundo", "jc.hdez.1294@gmail.com", "/Users/jeanca/Desktop/test/image-0bf45ca6-43cb-4e4b-8a03-523d6172d4fe.png")
+    if attach_mail_status:
+        print("Correo con adjunto enviado exitosamente.")
 main()
